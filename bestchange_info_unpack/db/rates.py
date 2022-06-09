038bd497a2c2dc78647afba3a043fe05b626c8f6
prudn <prundikov21@icloud.com>
2022-06-09 21:44:27 +0300

    fix data update + autoremove old data

--- bestchange_info_unpack/db/rates.py
+++ bestchange_info_unpack/db/rates.py
@@ -1,3 +1,4 @@
+import datetime
 import time
 
 from bestchange_info_unpack.db.base import sync_create_con
@@ -5,8 +6,36 @@ from bestchange_info_unpack.db.base import sync_create_con
 
 def update_rate():
     con, cur = sync_create_con()
-    cur.execute('INSERT ignore INTO rates (base_currency, target_currency, exchange_id, buy_price, sale_price, volume, min_order_size, max_order_size, city_id) '
-                'SELECT base_currency, target_currency, exchange_id, buy_price, sale_price, volume, min_order_size, max_order_size, city_id FROM pure_rates ')
+    cur.execute('INSERT INTO rates'
+        '(base_currency,'
+         'target_currency,'
+         'exchange_id,'
+         'buy_price,'
+         'sale_price,'
+         'volume,'
+         'min_order_size,'
+         'max_order_size,'
+         'city_id,'
+         'updated_at) '
+'  SELECT base_currency,'
+         'target_currency,'
+         'exchange_id,'
+         'buy_price,'
+         'sale_price,'
+         'volume,'
+         'min_order_size,'
+         'max_order_size,'
+         'city_id,'
+         'CURRENT_TIMESTAMP()'
+  'FROM pure_rates '
+'ON DUPLICATE KEY UPDATE '
+     'rates.buy_price = pure_rates.buy_price,'
+     'rates.sale_price = pure_rates.sale_price,'
+     'rates.volume = pure_rates.volume,'
+     'rates.min_order_size = pure_rates.min_order_size,'
+     'rates.max_order_size = pure_rates.max_order_size,'
+     'rates.updated_at = CURRENT_TIMESTAMP() ')
+    cur.execute(f'DELETE FROM rates WHERE updated_at < (NOW() - INTERVAL 2 MINUTE)')
     con.commit()
     con.close()
 
